47a66ee534bee52b345f62f993c0b2bb473fb4f8
Matt Schwartz <mschwartz@litmus.com>
2016-05-11 14:23:53 -0700

    Fix error in mail queue name

--- lib/sendwithus_ruby_action_mailer/jobs/mail_job.rb
+++ lib/sendwithus_ruby_action_mailer/jobs/mail_job.rb
@@ -5,7 +5,7 @@ module SendWithUsMailer
     # The <tt>SendWithUsMailer::Jobs::MailJob</tt> class is used when you
     # want to send transactional emails outside of the request-response cycle.
     class MailJob < ActiveJob::Base
-      queue_as :mailer
+      queue_as :mailers
 
       def perform(email_id, to, options)
         SendWithUs::Api.new.send_email(email_id, to, options)
